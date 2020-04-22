55671c226167d7a9c22f36507e7964b6cc694470
tusharkanchan <melayertush@gmail.com>
2020-04-22 15:20:12 +0530

    footer added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { DashboardService } from './dashboard.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable, observable } from 'rxjs';
+import { log } from 'util';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+   
+  constructor(
+    private dashboard:DashboardService
+  ) { }
 
-  constructor() { }
+  ngOnInit(){
+    }
 
-  ngOnInit(): void {
-  }
-
+  
+  
 }
