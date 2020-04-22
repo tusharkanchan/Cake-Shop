21ccec5fa5d600ebc8d4b90db7e2f5cb0b5b9b71
tusharkanchan <melayertush@gmail.com>
2020-04-22 12:47:28 +0530

    carosuel and cards implemented

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class DashboardRoutingModule { }
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,14 @@
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+
+
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,DashboardRoutingModule
+  ]
+})
+export class DashboardModule { }
